2bcaa38517172e40c056fcd52fb3e47691598cda
Musaddiqur Rahman <mashrur749@gmail.com>
2019-10-13 10:18:57 -0400

    footer added

--- src/Palette.js
+++ src/Palette.js
@@ -29,10 +29,10 @@ export default class Palette extends Component {
 
   render() {
     
-    const {colors} = this.props.palette;
+    const {colors, paletteName, emoji} = this.props.palette;
     const {level, format} = this.state;
     const colorBoxes = colors[level].map(color => (    
-        <ColorBox background = {color[format]} name = {color.name}/>
+        <ColorBox key={color.id} background = {color[format]} name = {color.name}/>
       ))
     return (
       <div className = "Palette">
@@ -40,6 +40,10 @@ export default class Palette extends Component {
         <div className="Palette-colors">
           {colorBoxes}
         </div>
+        <footer className="Palette-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     )
   }
