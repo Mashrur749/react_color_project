5bafbb267fca8c6181ff68fc10c8198b397e8fa8
Musaddiqur Rahman <mashrur749@gmail.com>
2019-10-15 16:59:01 -0400

    start styling paletteList

---
+++ src/MiniPalette.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {withStyles} from "@material-ui/styles";
+
+const styles = {
+    root:{
+        backgroundColor: "white",
+        border: "1px solid black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        position: "relative",
+        overflow: "hidden",
+        "&:hover": {
+            cursor: "pointer"
+        }
+    },
+    colors: {
+        backgroundColor: "grey"
+    },
+    title: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "0",
+        color: "black",
+        paddingTop: ".5rem",
+        fontSize: "1rem",
+        position:"relative"
+
+    },
+    emoji: {
+        marginLeft: ".5rem",
+        fontSize: "1.5rem"
+    }
+}
+
+const MiniPalette = (props) => {
+    const {classes, emoji, paletteName} = props;
+    return (
+        <div className={classes.root}>
+            <div className={classes.colors}></div>
+            <h5 className={classes.title}> {paletteName} <span>{emoji}</span> </h5>
+        </div>
+    );
+}
+
+export default withStyles(styles)(MiniPalette);
+
